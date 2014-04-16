b7074d2aba0538a3f5365e7fdb7aab3e468c8799
Oliver Thane <othane@gmail.com>
2014-04-16 17:32:55 +1200

    add optional message
    add an optional message to pif broadcasts that will be displayed on a
    search (next to the ip address), so a user can find a specific targets ip
    address by associating a unique string with each target.
    
    this resolves #2

--- pif.py
+++ pif.py
@@ -10,7 +10,7 @@ import time
 import argparse
 from daemon import Daemon
 
-def chime(timeout, iface=None):
+def chime(timeout, iface=None, msg = ''):
 	# deamonise and send a pif message at timeout rate
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	if iface != None:
@@ -18,18 +18,20 @@ def chime(timeout, iface=None):
 	s.bind(('', 0))
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 	while True:
-		msg = MSG
-		s.sendto(msg, ('<broadcast>', PORT))
+		m = MSG + msg
+		s.sendto(m, ('<broadcast>', PORT))
 		time.sleep(timeout)
 
-class ChimeDaemon(Daemon):
+class ChimeDaemon(object,Daemon):
 	timeout = 2
+	msg = ''
 	def run(self):
-		chime(self.timeout)
+		chime(self.timeout, self.msg)
 
-def daemon(cmd, timeout):
+def daemon(cmd, timeout, msg):
 	daemon = ChimeDaemon('/tmp/chime-daemon.pid')
 	daemon.timeout = timeout
+	daemon.msg = msg
 	print "daemon made"
 	if cmd == 'start':
 		print "starting"
@@ -57,8 +59,12 @@ def search(timeout, iface=None):
 		try:
 			if select.select([s], [], [], 1)[0]:
 				msg, addr = s.recvfrom(32)
-				if msg == MSG:
-					print "pif found: " + str(addr[0])
+				if MSG in msg:
+					msg = msg.split(MSG)
+					if len(msg) > 1 and len(msg[1]) > 0:
+						print "pif found: %s: %s" % (str(addr[0]), msg[1])
+					else:
+						print "pif found: %s" % (str(addr[0]))
 			now = time.time()
 			if now > end_time:
 				break
@@ -70,24 +76,28 @@ def main():
 	parser.add_argument('-t', '--timeout', help='timeout for the operation, for search this is the time to search, for chime this is the delay between chimes')
 	parser.add_argument('-s', '--search', action='store_true', help='search for pif devices')
 	parser.add_argument('-c', '--chime', action='store_true', help='broadcast pif message with ipaddress at regular intervals')
+	parser.add_argument('-m', '--message', nargs=1, help='add a message to the pif chime that will be shown with the device when found in a search')
 	parser.add_argument('-d', '--daemon', nargs=1, help='[start|stop|restart] chime processes as a deamon')
 	parser.add_argument('-i', '--interface', nargs=1, help='interface to listen or broadcast too')
 	args = parser.parse_args()
 	timeout = 2
 	iface = None
+	msg = ''
 	if args.timeout:
 		timeout = int(args.timeout)
+	if args.message:
+		msg = args.message[0]
 	if args.interface:
 		iface = args.interface[0]
 		print "using interface " + iface
 	if args.search:
 		search(timeout, iface)
 	elif args.daemon:
-		if not daemon(args.daemon[0], timeout):
+		if not daemon(args.daemon[0], timeout, msg):
 			print "error: daemon options are [start|stop|restart]"
 			parser.print_help()
 	elif args.chime:
-		chime(timeout, iface)
+		chime(timeout, iface, msg)
 	else:
 		print "error: no option specified"
 		parser.print_help()
